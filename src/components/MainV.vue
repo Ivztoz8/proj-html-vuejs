42b0a1aaa9354f189a04ad4f34adcc1e2b50008a
Ivztoz8 <ivan.toselli31@gmail.com>
2022-04-29 18:19:36 +0200

    Stp 2a: Main section work in progress..

--- src/components/MainV.vue
+++ src/components/MainV.vue
@@ -1,12 +1,88 @@
 <template>
-  <div class="container-fluid bg-cl-charade d-flex justify-content-center flex-column">
+  <div>
+    <!-- Sezione  -->
+    <section>
+      <div class="container-fluid bg-cl-charade d-flex justify-content-center flex-column">
     <div class="row bg-cl-mandy py-2 line-container align-self-center"></div>
     <div class="row py-5 mt-5">
       <h2 class="text-white font0H fw-bold text-center">Music is Life</h2>
     </div>
-    <div class="border-top border-secondary py-2 text-center line-container1 align-self-center">
+    <div class="border-top border-secondary py-2 text-center line-container1 align-self-center"></div>
+    <div class="row py-5 text-center">
+      <p class="text-secondary"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima tenetur ducimus eligendi,
+        repellendus nisi facere dicta molestias </p><p class="text-secondary"> sequi at a ut consectetur nam aut minus numquam omnis, 
+        illum laudantium nulla. </p>
     </div>
-    <div class="row border border-warning py-5 text-center"></div>
+  </div>
+  </section>
+  
+  <section>
+    <div class="container-fluid bg-cl-charade-mid d-flex justify-content-center flex-column">
+      <div class="row py-5 mt-5">
+        <h2 class="text-white font0H fw-bold text-center">Latest Band News</h2>
+      </div>
+      <div class="border-top border-secondary py-2 text-center line-container1 align-self-center"></div>
+      <div class="row py-5 text-center">
+        <p class="text-secondary"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima tenetur ducimus eligendi,
+          repellendus nisi facere dicta molestias </p><p class="text-secondary"> sequi at a ut consectetur nam aut minus numquam omnis, 
+          illum laudantium nulla. </p>
+    </div>
+    <div class="d-flex container-md border border-light">
+      <div class=" row-8">
+        <div class="col-12">
+          <div class="card" style="width: 100%; height: 25%;">
+            <img src="../assets/img/blog_music_techo.jpg" class="card-img-top img-fluid" alt="...">
+            <div class="card-body bg-secondary">
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-12 d-flex justify-content-center">
+          <div class="card" style="width: 25%; height: 50%;">
+            <img src="../assets/img/blog-post3.jpg" class="card-img-top img-fluid" alt="...">
+            <div class="card-body">
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+          </div>
+          <div class="card" style="width: 25%; height: 50%;">
+            <img src="../assets/img/blog-post4.jpg" class="card-img-top img-fluid" alt="...">
+            <div class="card-body">
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-12">
+          <div class="card" style="width: 100%; height: 25%;">
+            <img src="../assets/img/blog_flavor_rock.jpg" class="card-img-top img-fluid" alt="...">
+            <div class="card-body">
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+          </div>
+        </div>
+
+      </div>
+      <div class="row-4">
+        <div class="col-12">
+           <div class="card" style="width: 100%;">
+            <img src="../assets/img/blog-post1.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-12">
+           <div class="card" style="width: 100%;">
+            <img src="../assets/img/blog-post2.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    </div>
+  </section>
+    
   </div>
 </template>
 
